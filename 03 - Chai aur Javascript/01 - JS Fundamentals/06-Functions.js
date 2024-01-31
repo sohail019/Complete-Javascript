53dc99dd4569e2c48c788407603e18ab923a2df9
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-01-31 19:20:13 +0000

    Learned more concepts about function such as function return statement and function expression

--- 03 - Chai aur Javascript/01 - JS Fundamentals/06-Functions.js
+++ 03 - Chai aur Javascript/01 - JS Fundamentals/06-Functions.js
@@ -18,108 +18,135 @@
 /* Define Example */
 
 function printHelloWorldThrice() {
-    console.log('Hello World')
-    console.log('Hello World')
-    console.log('Hello World')
+  console.log('Hello World')
+  console.log('Hello World')
+  console.log('Hello World')
+}
+
+/* RUN  Syntax
+
+func_name() // run once
+func_name() // run again
+*/
+
+/* Run Example */
+
+printHelloWorldThrice() // print 3 times hello world
+
+printHelloWorldThrice() // again, print 3 times hello world
+
+// No Inputs? -> right now, above example of function accepts zero inputs. They behave the same way every time
+
+// greet() -> Hi
+// greet() -> Hi
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* ARGUMENTS 
+  We can also write functions that accept inputs, called Arguments
+
+  greet('Sohail') -> Hi Sohail
+  greet('Sohail Shaikh') -> Hi Sohail Shaikh
+
+  sum(2+4) -> 6
+*/
+
+/* We've Seen this before  */
+
+// No Input Function
+let msg = 'hello'
+console.log(msg.toUpperCase()) //  HELLO 
+
+// Arguments
+console.log(msg.indexOf('h')) // 0
+console.log(msg.at(3)) // l
+
+
+// +++++++++++++++++++++++++++++++++++++++
+
+function greet(person) {
+  console.log(`Hi, ${person}`)
+}
+greet('Sohail')
+greet('Sohail Shaikh')
+
+/* Two Arguments  */
+
+function findLargest(x,y) {
+  if(x > y) {
+    console.log(`${x} is Larger than ${y}`)
+  } else if (x < y) {
+    console.log(`${y} is Larger than ${x}`)
+  } else {
+    console.log(`${x} and ${y} are equal`)
   }
-  
-  /* RUN  Syntax
-  
-  func_name() // run once
-  func_name() // run again
-  */
-  
-  /* Run Example */
-  
-  printHelloWorldThrice() // print 3 times hello world
-  
-  printHelloWorldThrice() // again, print 3 times hello world
-  
-  // No Inputs? -> right now, above example of function accepts zero inputs. They behave the same way every time
-  
-  // greet() -> Hi
-  // greet() -> Hi
-  // ++++++++++++++++++++++++++++++++++++++++++++++++++
-  
-  /* ARGUMENTS 
-    We can also write functions that accept inputs, called Arguments
-  
-    greet('Sohail') -> Hi Sohail
-    greet('Sohail Shaikh') -> Hi Sohail Shaikh
-  
-    sum(2+4) -> 6
-  */
-  
-  /* We've Seen this before  */
-  
-  // No Input Function
-  let msg = 'hello'
-  console.log(msg.toUpperCase()) //  HELLO 
-  
-  // Arguments
-  console.log(msg.indexOf('h')) // 0
-  console.log(msg.at(3)) // l
-  
-  
-  // +++++++++++++++++++++++++++++++++++++++
-  
-  function greet(person) {
-    console.log(`Hi, ${person}`)
-  }
-  greet('Sohail')
-  greet('Sohail Shaikh')
-  
-  /* Two Arguments  */
-  
-  function findLargest(x,y) {
-    if(x > y) {
-      console.log(`${x} is Larger than ${y}`)
-    } else if (x < y) {
-      console.log(`${y} is Larger than ${x}`)
-    } else {
-      console.log(`${x} and ${y} are equal`)
-    }
-  }
-  
-  findLargest(5, 6)
-  findLargest(9, 5)
-  findLargest(9, 9)
-  
-  
-  /* Return  
-   -> Built in methods return values when we call them. 
-   -> We can store those values
-  */
-  
-  console.log('Hello Sohail'.toUpperCase()) // HELLO SOHAIL
-  console.log('Hello Sohail'.at(7)) // o
-  
-  /*  No Return! 
-    -> Our functions print values out, but do NOT return anything 
-  */
-  
-  function add(x, y) {
-    console.log(x + y)
-  }
-  
-  const sum = add(10, 56)
-  console.log(sum) // undefined
-  
-  /*  FIRST RETURN 
-    -> Now we can capture a return value in a variable!
-  
-  */
-  
-  function addTwoNums(x,y) {
-    return x + y
-  }
-  
-  const sumTwoNums = addTwoNums(45, 65) 
-  console.log(sumTwoNums)
-  
-  
-  /*  RETURN -> 
-    ->The return statements ends function execution AND specifies the value to be returned by that function
-  */
-  
-  
+}
+
+findLargest(5, 6)
+findLargest(9, 5)
+findLargest(9, 9)
+
+
+/* Return  
+ -> Built in methods return values when we call them. 
+ -> We can store those values
+*/
+
+console.log('Hello Sohail'.toUpperCase()) // HELLO SOHAIL
+console.log('Hello Sohail'.at(7)) // o
+
+/*  No Return! 
+  -> Our functions print values out, but do NOT return anything 
+*/
+
+function add(x, y) {
+  console.log(x + y)
+}
+
+const sum = add(10, 56)
+console.log(sum) // undefined
+
+/*  FIRST RETURN 
+  -> Now we can capture a return value in a variable!
+
+*/
+
+function addTwoNums(x,y) {
+  return x + y
+}
+
+const sumTwoNums = addTwoNums(45, 65) 
+console.log(sumTwoNums)
+
+
+/*  RETURN -> 
+  -> The return statement ends with function execution AND specifies the value returned by that function
+
+  -> it means wo end me aaega
+*/
+
+function addThreeNums(x,y,z) {
+  console.log('Heloooo')
+  return x + y + z
+  // console.log('Heyyyy I'm console after return') // error
+}
+
+const sumThreeNums = addThreeNums(50,56,78)
+console.log(sumThreeNums)
+
+
+// --------------------------------------------------
+
+/* Function Expressions */
+
+// -> Here, ham koi function ki value variable mein store kar sakte hai
+// ->  Main Define -> Function can be assigned to a variable
+
+const square = function(num) {
+  return num * num
+}
+
+console.log(square(2)) // 4
+
+
+
+/* Functions are Objects */
