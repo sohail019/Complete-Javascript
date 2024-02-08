d2d4a6acff4bb1d76393fd15065e2631f70f0eb8
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-02-08 15:15:42 +0000

    Learned about 'this' keyword use in normal and arrow function

--- 03 - Chai aur Javascript/01 - JS Fundamentals/11-Normal-func-vs-Arrow-func.js
+++ 03 - Chai aur Javascript/01 - JS Fundamentals/11-Normal-func-vs-Arrow-func.js
@@ -1,3 +1,6 @@
+/* This behavior became very dangerous if we use var, var actually creates property in global object */
+var firstName = 'sohail'
+
 const sohail = {
   firstName: 'sohail',
   year: 2002,
@@ -16,4 +19,22 @@ const sohail = {
 
 sohail.calcAge() // 22
 sohail.greet() // Hey, undefined
-// Undefined because this is not code block, and all are in global.. and pointing to the global there is no firstName declared, that's why it returns undefined
+// Undefined because this is not code block, and all are in global.. and pointing to the global there is no firstName declared, that's why it returns 
+
+console.log(this) // window object
+console.log(this.firstName) // undefined
+
+
+
+/* Hence, we should never ever use  an arrow function as a method, always use normal function expression*/
+
+// ---------------------------- TRYING WITH NORMAL FUNCTION SAME EXAMPLE
+const sohail2 = {
+  firstName: 'sohail',
+  greet: function () {
+    console.log(`Hey, My name is ${this.firstName}`) // this will take firstName property from sohail2 object
+  }
+}
+sohail2.greet() // this will print : Hey, My name is sohail
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
