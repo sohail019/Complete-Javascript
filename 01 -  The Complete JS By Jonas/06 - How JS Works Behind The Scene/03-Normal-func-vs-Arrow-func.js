5ee04f6386d4ba5539a51a8eb3da8d21c3ee8ff6
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-02-08 17:36:46 +0000

    Learned about where to use Normal function and Arrow functions

--- 01 -  The Complete JS By Jonas/06 - How JS Works Behind The Scene/03-Normal-func-vs-Arrow-func.js
+++ 01 -  The Complete JS By Jonas/06 - How JS Works Behind The Scene/03-Normal-func-vs-Arrow-func.js
@@ -38,3 +38,71 @@ const sohail2 = {
 sohail2.greet() // this will print : Hey, My name is sohail
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+/* One Final Example of a pitfall of 'this' keyword is when we have function inside of a method*/
+console.log('********************************************')
+const newSohail = {
+  firstName: 'sohail',
+  year: 2002,
+
+  calcAge: function(birthYear) {
+    console.log(this)
+    console.log(2024 - this.year)
+
+
+    /* Solution 1 */
+    console.log('------------With Normal---------------')
+    const self = this // self or that
+    const isMillenialWithNormal = function () {
+      console.log(self)
+      console.log(self.year >= 2000 && self.year <= 2010) // it returns true
+
+      // console.log(this.year >= 2000 && this.year <= 2010) // it returns false
+    }
+    isMillenialWithNormal()
+
+  console.log('------------With Arrow---------------')
+    /* Solution 2 */
+    const isMillenialWithArrow = () => {
+      console.log(this)
+      console.log(this.year >= 2000 && this.year <= 2010) // it returns false
+    }
+    isMillenialWithArrow() // this worked because this arrow function uses the 'this' keyword uses it's parent scope
+  },
+
+  greet: () => {
+    console.log(`Hey, ${this.firstName}`)
+  }
+}
+
+newSohail.calcAge()
+
+// console.log(newSohail)
+
+/* arguments Keyword 
+
+  -> We learned that functions also gets access to an 'arguments' keyword, so not just 'this' keyword, but also an arguments keyword.
+  -> Just like the 'this' keyword, the arguments keyword is only available in regular functions.
+  ->
+*/
+console.log('+++++++++++++Argument Keyword+++++++++++++++++')
+const addExpr = function(a,b) {
+  console.log(arguments)
+  return a + b
+}
+
+addExpr(3,8) // we get an array with 3 and 8, that's exactly two parameters that we passed in. (2 parameters----> 2 arguments)
+
+addExpr(3,8,5,4,7) // this can be useful when we pass more parameters than we actually specified (2 parameters----> more than 2 arguments)
+
+
+/* But what if we do this in Arrow Function */
+const addArrow = (a,b) => {
+  // console.log(arguments) // ReferenceError: arguments is not defined
+  return a + b 
+}
+
+addArrow()
+
+// NOTE - arguments keyword exist, but only exist in regular functions
