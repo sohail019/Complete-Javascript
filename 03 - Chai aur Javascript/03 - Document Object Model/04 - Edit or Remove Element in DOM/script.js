e329e1b2c320a4bd1532a098d629a6571165ed27
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-02-26 07:09:00 +0000

    Learned about every approach to edit on DOM

--- 03 - Chai aur Javascript/03 - Document Object Model/04 - Edit or Remove Element in DOM/script.js
+++ 03 - Chai aur Javascript/03 - Document Object Model/04 - Edit or Remove Element in DOM/script.js
@@ -33,3 +33,41 @@ const addLangWithOpti = (langName) => {
   document.querySelector('.languages').appendChild(li)
 }
 addLangWithOpti('c++')
+
+// ***************************************************
+
+/* // Edit values in DOM  */
+
+// edit with innerHTML.. from python to ruby
+const secondLang = document.querySelector('li:nth-child(2)')
+secondLang.innerHTML = 'Ruby' // this will work but it's not optimized for larger scale
+
+// ---------------------------------------------
+
+// edit with textContent.. from java to python
+const thirdLang = document.querySelector('li:nth-child(3)')
+thirdLang.textContent = 'Python'  // this will also works but not optimized yet
+
+// ---------------------------------------------
+
+// edit with innerText.. 1st li from javascript to Java
+const firstLang = document.querySelector('li:first-child')
+firstLang.innerText = 'Java' // works but not optimized
+
+// ---------------------------------------------
+
+// edit with outerHTMl.. last value c++ tp c#
+const lastLang = document.querySelector('li:last-child')
+lastLang.outerHTML = '<li>C#</li>' // sabse bakwas
+
+// ---------------------------------------------
+
+/* // So what is optimized solution?? */
+// replace 4th li from Typescript to Javascript 
+const fourthLang = document.querySelector('li:nth-child(4)')
+
+// create new li item
+const newli = document.createElement('li')
+newli.textContent = 'Javascript'
+fourthLang.replaceWith(newli) // this is optimized way
+
