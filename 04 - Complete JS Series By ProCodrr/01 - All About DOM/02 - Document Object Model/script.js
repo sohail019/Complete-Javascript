ce8b46084e1650de668cfa1ccc2d88cee20ddfae
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-03-10 13:24:41 +0000

    Learned about DOM, Document Object properties, manipulation, finding HTML objects

---
+++ 04 - Complete JS Series By ProCodrr/01 - All About DOM/02 - Document Object Model/script.js
@@ -0,0 +1,29 @@
+/*  JavaScript HTML DOM Document
+    --> The HTML DOM document object is the owner of all other objects in your webpage
+
+
+    -> HTML DOM document Object
+        --> The 'document' object represents your webpage
+        --> If you want to access any element in an HTML page, you always start with accessing the document oject first
+*/
+
+
+/* Finding HTML Objects */
+console.log(document) // HTML return karega
+console.dir(document) // Object (Key:Value Pair Return Karega)
+
+
+console.log(document.baseURI) // Returns the absolute base URI of the document
+
+console.log(document.doctype) // Returns the Document Type
+
+console.log(document.documentElement) // Returns the <html> element
+
+console.log(document.domain) //  Returns the Domain Name of the Document Server
+
+console.log(document.images) // Returns All Images
+
+console.log(document.links) // Returns all <a>
+
+console.log(document.URL) // Returns the complete URL  of the document
+
