1ede28f7e941997f981c594d4c85670db2e2ae3a
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-25 06:53:36 +0000

    Completed another project for keyboard press

---
+++ 03 - Chai aur Javascript/07 - Async JS/02 - Project 2 - Keyboard Check/script.js
@@ -0,0 +1,23 @@
+const insert = document.querySelector('#insert')
+
+window.addEventListener('keydown', function(e){
+
+    insert.innerHTML = `
+    
+    <div class="color">
+        <table>
+            <tr>
+                <th>Key</th>
+                <th>Key Code</th>
+                <th>Code</th>
+            </tr>
+            <tr>
+                <td>${e.key === " " ? "Space" : e.key}</td>
+                <td>${e.keyCode}</td>
+                <td>${e.code}</td>
+            </tr>
+        </table>
+    
+    </div>
+    `
+})
